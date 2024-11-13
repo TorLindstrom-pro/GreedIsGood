da59f0bdf33be7afc17e94dc6271b900b411dc7a
TorLindstrom <tor.lindstrom@winlas.se>
2024-11-13 09:24:51 +0100

    fix: rename test

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -9,7 +9,7 @@ public class UnitTest1
     [InlineData(5, 50)]
     [InlineData(2, 0)]
     [InlineData(3, 0)]
-    public void Score_ReturnsANumber(int dice, int expectedScore)
+    public void Score_SingleDiceGivesCorrectScores(int dice, int expectedScore)
     {
         var result = Kata.Score([dice]);
         Assert.Equal(expectedScore, result);
