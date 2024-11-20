ded95ec006eb02abd2cb09b16ee2e05ed987f2d1
TorLindstrom <tor.lindstrom@winlas.se>
2024-11-20 09:09:06 +0100

    refactor: use single linq expression

--- GreedIsGood/Class1.cs
+++ GreedIsGood/Class1.cs
@@ -1,38 +1,40 @@
-﻿namespace GreedIsGood;
+﻿using static System.Math;
+
+namespace GreedIsGood;
 
 public static class Kata
 {
-	public static int Score(int[] dice)
+	public static int Score(int[] dice) => dice
+			.GroupBy(die => die)
+			.Select(group =>
+			{
+				var (tripletCount, singlesCount) = DivRem(group.Count(), 3);
+				
+				var tripletsScore = ScoreTriplets(group.Key, tripletCount);
+				var singlesScore = ScoreOfDie(group.Key, singlesCount);
+				
+				return tripletsScore + singlesScore;
+			})
+			.Sum();
+
+	private static int ScoreTriplets(int die, int tripletCount)
 	{
-		var diceGroups = dice.GroupBy(die => die);
-
-		var tripletsScore = diceGroups
-			.Where(group => group.Count() >= 3)
-			.Sum(group => ScoreTriplets(group.Key));
-
-		var singlesScore = diceGroups
-			.Select(group => (group.Count() % 3, group.Key) )
-			.Sum(group => ScoreOfEachDie(group.Key) * group.Item1);
-
-		return tripletsScore + singlesScore;
-	}
-
-	private static int ScoreTriplets(int die)
-	{
-		return die switch
+		var score = die switch
 		{
 			1 => 1000,
 			_ => die * 100
 		};
+		return score * tripletCount;
 	}
 
-	private static int ScoreOfEachDie(int die)
+	private static int ScoreOfDie(int die, int singlesCount)
 	{
-		return die switch
+		var score = die switch
 		{
 			1 => 100,
 			5 => 50,
 			_ => 0
 		};
+		return score * singlesCount;
 	}
 }
